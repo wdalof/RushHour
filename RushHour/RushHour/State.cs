1299c965b5343ded2e3765b096f4530e345dfcaa
Wim Dalof <wdalof@hotmail.com>
2014-04-11 14:20:18 +0200

    Added some class files

---
+++ RushHour/RushHour/State.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RushHour
+{
+    class State
+    {
+        RushHourBoard _board;
+        State _parent;
+
+            public State(RushHourBoard board, State parent){
+                _board = board;
+                _parent = parent;
+            }
+    }
+}
