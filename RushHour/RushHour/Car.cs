623cc2faf06fac879d12609a067317e1850aa31d
Wim Dalof <wdalof@hotmail.com>
2014-04-11 13:53:34 +0200

    Base code (from e-mail)

---
+++ RushHour/RushHour/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RushHour
+{
+    class Car
+    {
+        int length;
+        int x;
+        int y;
+        Direction direction;
+
+        public enum Direction
+        {
+            HORIZONTAL,VERTICAL
+        }
+
+        public Car(int x,int y,int length, Car.Direction direction)
+        {
+            this.x = x;
+            this.y = y;
+            this.length = length;
+            this.direction = direction;
+        }
+
+        public Direction getDirection()
+        {
+            return direction;
+        }
+
+        public int getX()
+        {
+            return x;
+        }
+    }
+
+   
+}
---
+++ RushHour/RushHour/RushHourBoard.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RushHour
+{
+    class RushHourBoard
+    {
+        public static int size = 6;
+        private int width;
+        private int height;
+        char[,] board;
+
+        public RushHourBoard()
+        {
+            width = size;
+            height = size;
+            initBoard();
+        }
+
+        public void initBoard(){
+            board = new char[size, size];
+
+            //set all x and y values to . when spots are taken we change their values to a char responding the vehicle
+            for (int k = 0; k < board.GetLength(0); k++)
+                for (int l = 0; l < board.GetLength(1); l++)
+                    board[k, l] = '.';
+
+            //print board - test
+            for (int k = 0; k < board.GetLength(0); k++)
+                for (int l = 0; l < board.GetLength(1); l++)
+                    Console.WriteLine(board[k, l]);
+                    Console.ReadLine();
+                    
+        }
+
+
+    }
+}
